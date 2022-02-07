d2a9f48047b9e6521a61774e12d4e9b76e5a6ee8
pang yew ken <ukenster@gmail.com>
2022-02-07 18:29:53 +0800

    completed 20 valid parentheses

--- 020-valid-parentheses/app.rb
+++ 020-valid-parentheses/app.rb
@@ -3,17 +3,25 @@
 require 'pry-byebug'
 
 def is_valid(s)
+  return false if s.length < 2 || s.length.odd?
   p "check validity on #{s}"
   parentheses = ['(', '[', '{', ')', ']', '}']
-  s = s.chars.select{ |ltr| parentheses.include?(ltr)}
+  s = s.chars.select{ |ltr| parentheses.include?(ltr)}.join
   h = {
     '(' => ')',
     '[' => ']',
     '{' => '}'
   }
+  5000.times do
+      s = s.gsub(/\[\]|\(\)|\{\}/, "")
+  end
+  s = s.gsub(/\[\]|\(\)|\{\}/, "").chars
+  p s
+  return true if s.count == 0
 
   until s.count == 0
     # If Closing tag second char
+    return false if h.keys.include?(s[0])
     if h.values.include?(s[1])
       # p "closing tag second char"
       if h[s[0]] == s[1]
@@ -51,7 +59,8 @@ end
 # h["("] == h[")"] && h["["] == h["]"] && h["{"] == h["}"]
 
 # p is_valid("(word)")
-# p is_valid("()[]{}")
+p is_valid("()[]{}")
 # p is_valid("(]")
-# p is_valid("{[]}")
-p is_valid("([)]")
+p is_valid("{[]}")
+# p is_valid("([)]")
+# p is_valid("(([]){})")
