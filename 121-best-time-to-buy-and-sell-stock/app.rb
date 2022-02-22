7e80c1123e07b7f62ff38f27ac93dfe9a09453a7
pang yew ken <ukenster@gmail.com>
2022-02-22 15:55:55 +0800

    Starting leet-code 121 best time to buy stocks

---
+++ 121-best-time-to-buy-and-sell-stock/app.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+
+end
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+
+# Constraints:
+# 1 <= prices.length <= 105
+# 0 <= prices[i] <= 104
