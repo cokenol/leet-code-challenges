8e5b25808627afe59dde90cb2e8a7664c4937ae5
pang yew ken <ukenster@gmail.com>
2022-02-25 19:28:33 +0800

    Added fastest method found for 217-contains-duplicates

---
+++ 217-contains-duplicates/fastest.js
@@ -0,0 +1,14 @@
+/*
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let seen = new Set()
+
+  for (const num of nums) {
+    if (seen.has(num)) return true
+    else seen.add(num)
+  }
+
+  return false
+}
