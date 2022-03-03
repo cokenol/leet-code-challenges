34375e88c7474cd2c26191e4a2d83a5e290635c6
pang yew ken <ukenster@gmail.com>
2022-03-03 17:20:03 +0800

    Try 189 rotate array

---
+++ 189-rotate-array/app.js
@@ -0,0 +1,37 @@
+/*
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  next = 0
+  for (let i = 0; i < nums.length; i++) {
+    let swap = nums[-k - next]
+    nums[-k - next] = nums[i]
+    nums[i] = swap
+    next++
+  }
+  return nums
+}
+
+// Example 1:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+// Constraints:
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 1))
