b0fc8254b2b147e024a489104659577640b05d70
pang yew ken <ukenster@gmail.com>
2022-03-19 15:26:48 +0800

    Corrected recursion formula

--- 070-climbing-stairs/dp.js
+++ 070-climbing-stairs/dp.js
@@ -11,7 +11,7 @@ var climbStairs = function (n) {
       return dp[n]
     } else {
       calculations++
-      dp[n] = climbS(n - 1) + climbS(n - 2) + climbS(n - 3)
+      dp[n] = climbS(n - 1) + climbS(n - 2)
       return dp[n]
     }
   }
@@ -19,23 +19,25 @@ var climbStairs = function (n) {
 
 const climb = climbStairs()
 
-for (let i = 0; i < 40; i++) {
-  console.log(i, climb(i))
+for (let i = 0; i <= 20; i++) {
+  console.log("For", i, "number of steps is=", climb(i), "distinct ways")
   console.log("calc=", calculations, "\n")
 }
 
 // n = 1
 // 1
+// 1 Distinct way
 
 // n = 2
 // 1 1
 // 2
+// 2 Distinct way
 
 // n = 3
 // 1 1 1
 // 2 1
 // 1 2
-// 3
+// 3 Distinct way
 
 // n = 4
 // 1 1 1 1
