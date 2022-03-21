3eba0093585209c1265dfaba69b6a82202c9203c
pang yew ken <ukenster@gmail.com>
2022-03-21 22:39:33 +0800

    Completed 202 is Happy Number

---
+++ 202-happy-number/app.js
@@ -0,0 +1,62 @@
+/*
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// Time = 85 ms, faster than 65.11% of JavaScript online submissions for Happy Number.
+// Space - 43.5 MB, less than 57.61% of JavaScript online submissions for Happy Number.
+
+var isHappy = function (n) {
+  let dp = {}
+  return function hap(n) {
+    let res = 0
+    // console.log(n, dp)
+    Array.from(String(n), Number).forEach(n => {
+      res += n ** 2
+    })
+    if (res === 1) {
+      dp = {}
+      return true
+    }
+    if (!dp[n]) {
+      dp[n] = res
+    } else {
+      return false
+    }
+    return hap(res)
+  }
+}
+
+const happy = isHappy()
+
+arr = [19, 7, 13, 3, 23]
+arr.forEach(n => {
+  console.log(happy(n))
+})
+/*
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+
+Example 1:
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+Example 2:
+Input: n = 2
+Output: false
+
+Constraints:
+1 <= n <= 231 - 1
+*/
