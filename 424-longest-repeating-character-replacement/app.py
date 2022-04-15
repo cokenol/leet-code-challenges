3aa9b620f4c79bbd159a03f2dd29d03c190f3254
pang yew ken <ukenster@gmail.com>
2022-04-15 21:48:58 +0800

    optimize solution to use left pointers and checking as integer

--- 424-longest-repeating-character-replacement/app.py
+++ 424-longest-repeating-character-replacement/app.py
@@ -1,23 +1,23 @@
-# Runtime: 127 ms, faster than 86.32% of Python3 online submissions for Longest Repeating Character Replacement.
+# Runtime: 82 ms, faster than 99.40% of Python3 online submissions for Longest Repeating Character Replacement.
 # Memory Usage: 14 MB, less than 60.99% of Python3 online submissions for Longest Repeating Character Replacement.
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        checking = ""
+        checking = 0
         count = {}
         longest = 0
+        maxf = 0
+        l = 0
         for char in s:
-            if char not in count:
-                count[char] = 1
-            else:
-                count[char] += 1
-            checking += char
-            if len(checking) - max(count.values()) > k:
-                count[checking[0]] -= 1
-                checking = checking[1:]
-            if longest < len(checking):
-                longest = len(checking)
-            print(char, checking, count, longest)
+            count[char] = 1 + count.get(char, 0)
+            maxf = max(maxf, count[char])
+            checking += 1
+            if checking - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+                checking -= 1
+            if longest < checking:
+                longest = checking
         return longest
 
 
